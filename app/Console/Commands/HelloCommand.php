49a06c2266c6b0e1df8e8b4ab301a246b2a10872
y-ugiyama654 <y.sugiyama54@gmail.com>
2019-11-21 00:41:47 +0900

    Initial Commit

---
+++ app/Console/Commands/HelloCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class HelloCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'hello:class';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'サンプルコマンド（クラス）';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->comment('Hello class command!');
+    }
+}
